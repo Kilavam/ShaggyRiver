368f2bda40e0778421852bbbdc0632d24d316b16
Mathieu Valiquette <mathieu.valique@gmail.com>
2019-06-29 23:34:03 -0400

    Dumb animation and movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float _speed = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var direction = new Vector2(
+            Input.GetAxis("Horizontal"),
+            Input.GetAxis("Vertical"));
+
+        var body = GetComponent<Rigidbody2D>();
+        body.velocity += direction * _speed * Time.deltaTime;
+    }
+}
---
+++ Assets/Scripts/ScratchAnimation.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScratchAnimation : MonoBehaviour
+{
+    public Sprite[] Frames;
+    public float FrameLife = 1; // in second;
+    private float currentFrameLife;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentFrameLife += Time.deltaTime;
+        if(currentFrameLife > FrameLife)
+        {
+            currentFrameLife = 0;
+            int frameCount = Frames.Length;
+            int nextFrameIndex = (int)(Random.value * frameCount);
+            var spriteRenderer = GetComponent<SpriteRenderer>();
+            spriteRenderer.sprite = Frames[nextFrameIndex];
+            spriteRenderer.flipX = Random.value > 0.5f;
+            spriteRenderer.flipY = Random.value > 0.5f;
+        }
+    }
+}
